3e47084c7af9f857e76ef9085537ad836c861504
Richard Willars <richard.willars@hivehome.com>
2017-04-28 14:39:34 +0100

    Extends React.component instead of React.createClass

--- src/Swipe.js
+++ src/Swipe.js
@@ -1,27 +1,16 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-const Swipe = React.createClass({
-    displayName: 'Swipe',
-    propTypes: {
-        nodeName: PropTypes.string,
-        mouseSwipe: PropTypes.bool,
-        className: PropTypes.string,
-        onSwipingUp: PropTypes.func,
-        onSwipingRight: PropTypes.func,
-        onSwipingDown: PropTypes.func,
-        onSwipingLeft: PropTypes.func,
-        onSwipedUp: PropTypes.func,
-        onSwipedRight: PropTypes.func,
-        onSwipedDown: PropTypes.func,
-        onSwipedLeft: PropTypes.func,
-        onSwipe: PropTypes.func,
-        delta: PropTypes.number,
-        preventDefaultEvent: PropTypes.bool,
-        style: PropTypes.object
-    },
-    getInitialState() {
-        return {
+class Swipe extends React.Component {
+    constructor() {
+        super();
+        this._mouseStart = this._mouseStart.bind(this);
+        this._mouseMove = this._mouseMove.bind(this);
+        this._mouseEnd = this._mouseEnd.bind(this);
+        this._touchStart = this._touchStart.bind(this);
+        this._touchMove = this._touchMove.bind(this);
+        this._touchEnd = this._touchEnd.bind(this);
+        this.state = {
             x: 0,
             y: 0,
             status: false,
@@ -29,25 +18,29 @@ const Swipe = React.createClass({
             delta: 50,
             preventDefaultEvent: false
         };
-    },
+    }
+
     render() {
         let newProps = {
             onTouchStart: this._touchStart,
             onTouchMove: this._touchMove,
             onTouchEnd: this._touchEnd,
-            className: this.props.className || "",
+            className: this.props.className || '',
             style: this.props.style || {}
         };
-        if (this.props.mouseSwipe){
+        if (this.props.mouseSwipe) {
             newProps.onMouseMove = this._mouseMove;
             newProps.onMouseDown = this._mouseStart;
             newProps.onMouseUp = this._mouseEnd;
         }
-        return React.createElement(this.props.nodeName || "div", newProps, this.props.children);
-    },
-    _mouseStart(e){
-        if(this.props.preventDefaultEvent && this.props.preventDefaultEvent!==this.state.preventDefaultEvent) this.setState({preventDefaultEvent: this.props.preventDefaultEvent});
-        if(this.props.delta && this.props.delta!==this.state.delta) this.setState({delta: this.props.delta});
+        return React.createElement(this.props.nodeName || 'div', newProps, this.props.children);
+    }
+
+    _mouseStart(e) {
+        if (this.props.preventDefaultEvent && this.props.preventDefaultEvent !== this.state.preventDefaultEvent) {
+            this.setState({ preventDefaultEvent: this.props.preventDefaultEvent });
+        }
+        if (this.props.delta && this.props.delta !== this.state.delta) this.setState({ delta: this.props.delta });
 
         this.setState({
             x: Math.abs(e.clientX).toFixed(2),
@@ -55,80 +48,75 @@ const Swipe = React.createClass({
             status: true,
             detected: false
         });
-        if(this.state.preventDefaultEvent) e.preventDefault();
-    },
-    _mouseMove(e){
-        if (this.state.status){
-            let x = (e.clientX).toFixed(2);
-            let y = (e.clientY).toFixed(2);
+        if (this.state.preventDefaultEvent) e.preventDefault();
+    }
+
+    _mouseMove(e) {
+        if (this.state.status) {
+            let x = e.clientX.toFixed(2);
+            let y = e.clientY.toFixed(2);
             let tX = parseFloat((x - this.state.x).toFixed(2));
             let tY = parseFloat((y - this.state.y).toFixed(2));
 
             if (Math.abs(tX) > Math.abs(tY) && this.props.onSwipe) this.props.onSwipe([tX, 0]);
             else if (Math.abs(tX) < Math.abs(tY) && this.props.onSwipe) this.props.onSwipe([0, tY]);
 
-            if(Math.abs(tX)>=this.state.delta){
-                if(tX>this.state.delta){
-                    if(this && this.props && this.props.onSwipingRight) this.props.onSwipingRight(true);
-                    if(this && this.props && this.props.right) this.props.right(true);
+            if (Math.abs(tX) >= this.state.delta) {
+                if (tX > this.state.delta) {
+                    if (this && this.props && this.props.onSwipingRight) this.props.onSwipingRight(true);
+                    if (this && this.props && this.props.right) this.props.right(true);
+                } else if (tX < -this.state.delta) {
+                    if (this && this.props && this.props.onSwipingLeft) this.props.onSwipingLeft(true);
+                    if (this && this.props && this.props.left) this.props.left(true);
                 }
-                else if(tX<(-this.state.delta)){
-                    if(this && this.props && this.props.onSwipingLeft) this.props.onSwipingLeft(true);
-                    if(this && this.props && this.props.left) this.props.left(true);
+            } else if (Math.abs(tY) >= this.state.delta) {
+                if (tY > this.state.delta) {
+                    if (this && this.props && this.props.onSwipingDown) this.props.onSwipingDown(true);
+                } else if (tY < -this.state.delta) {
+                    if (this && this.props && this.props.onSwipingUp) this.props.onSwipingUp(true);
+                    if (this && this.props && this.props.up) this.props.up(true);
                 }
             }
-            else{
-                if(Math.abs(tY)>=this.state.delta){
-                    if(tY>this.state.delta){
-                        if(this && this.props && this.props.onSwipingDown) this.props.onSwipingDown(true);
+
+            if (!this.state.detected) {
+                if (Math.abs(parseFloat(tX)) >= this.state.delta) {
+                    if (parseFloat(tX) > this.state.delta) {
+                        if (this && this.props && this.props.onSwipedRight) this.props.onSwipedRight(true);
+                        this.setState({ detected: true });
+                    } else if (parseFloat(tX) < -this.state.delta) {
+                        if (this && this.props && this.props.onSwipedLeft) this.props.onSwipedLeft(true);
+                        this.setState({ detected: true });
                     }
-                    else if(tY<(-this.state.delta)){
-                        if(this && this.props && this.props.onSwipingUp) this.props.onSwipingUp(true);
-                        if(this && this.props && this.props.up) this.props.up(true);
+                } else if (Math.abs(parseFloat(tY)) >= this.state.delta) {
+                    if (parseFloat(tY) > this.state.delta) {
+                        if (this && this.props && this.props.onSwipedDown) this.props.onSwipedDown(true);
+                        this.setState({ detected: true });
+                    } else if (parseFloat(tY) < -this.state.delta) {
+                        if (this && this.props && this.props.onSwipedUp) this.props.onSwipedUp(true);
+                        this.setState({ detected: true });
                     }
                 }
             }
 
-            if(!this.state.detected){
-                if(Math.abs(parseFloat(tX))>=this.state.delta){
-                    if(parseFloat(tX)>this.state.delta){
-                        if(this && this.props && this.props.onSwipedRight) this.props.onSwipedRight(true);
-                        this.setState({detected: true});
-                    }
-                    else if(parseFloat(tX)<(-this.state.delta)){
-                        if(this && this.props && this.props.onSwipedLeft) this.props.onSwipedLeft(true);
-                        this.setState({detected: true});
-                    }
-                }
-                else{
-                    if(Math.abs(parseFloat(tY))>=this.state.delta){
-                        if(parseFloat(tY)>this.state.delta){
-                            if(this && this.props && this.props.onSwipedDown) this.props.onSwipedDown(true);
-                            this.setState({detected: true});
-                        }
-                        else if(parseFloat(tY)<(-this.state.delta)){
-                            if(this && this.props && this.props.onSwipedUp) this.props.onSwipedUp(true);
-                            this.setState({detected: true});
-                        }
-                    }
-                }
-            }
-
-            if(this.state.preventDefaultEvent) e.preventDefault();
+            if (this.state.preventDefaultEvent) e.preventDefault();
         }
-    },
-    _mouseEnd(e){
+    }
+
+    _mouseEnd(e) {
         this.setState({
             x: 0,
             y: 0,
             status: false,
             detected: false
         });
-        if(this.state.preventDefaultEvent) e.preventDefault();
-    },
+        if (this.state.preventDefaultEvent) e.preventDefault();
+    }
+
     _touchStart(e) {
-        if(this.props.preventDefaultEvent && this.props.preventDefaultEvent!==this.state.preventDefaultEvent) this.setState({preventDefaultEvent: this.props.preventDefaultEvent});
-        if(this.props.delta && this.props.delta!==this.state.delta) this.setState({delta: this.props.delta});
+        if (this.props.preventDefaultEvent && this.props.preventDefaultEvent !== this.state.preventDefaultEvent) {
+            this.setState({ preventDefaultEvent: this.props.preventDefaultEvent });
+        }
+        if (this.props.delta && this.props.delta !== this.state.delta) this.setState({ delta: this.props.delta });
 
         this.setState({
             x: Math.abs(e.touches[0].pageX).toFixed(2),
@@ -136,71 +124,62 @@ const Swipe = React.createClass({
             status: true,
             detected: false
         });
-        if(this.state.preventDefaultEvent) e.preventDefault();
-    },
+        if (this.state.preventDefaultEvent) e.preventDefault();
+    }
+
     _touchMove(e) {
         if (e.touches.length > 1) {
-            return
+            return;
         }
 
-        let x = (e.touches[0].pageX).toFixed(2);
-        let y = (e.touches[0].pageY).toFixed(2);
+        let x = e.touches[0].pageX.toFixed(2);
+        let y = e.touches[0].pageY.toFixed(2);
         let tX = parseFloat((x - this.state.x).toFixed(2));
         let tY = parseFloat((y - this.state.y).toFixed(2));
 
         if (Math.abs(tX) > Math.abs(tY) && this.props.onSwipe) this.props.onSwipe([tX, 0]);
         else if (Math.abs(tX) < Math.abs(tY) && this.props.onSwipe) this.props.onSwipe([0, tY]);
 
-
-        if(Math.abs(tX)>=this.state.delta){
-            if(tX>this.state.delta){
-                if(this && this.props && this.props.onSwipingRight) this.props.onSwipingRight(true);
-                if(this && this.props && this.props.right) this.props.right(true);
+        if (Math.abs(tX) >= this.state.delta) {
+            if (tX > this.state.delta) {
+                if (this && this.props && this.props.onSwipingRight) this.props.onSwipingRight(true);
+                if (this && this.props && this.props.right) this.props.right(true);
+            } else if (tX < -this.state.delta) {
+                if (this && this.props && this.props.onSwipingLeft) this.props.onSwipingLeft(true);
+                if (this && this.props && this.props.left) this.props.left(true);
             }
-            else if(tX<(-this.state.delta)){
-                if(this && this.props && this.props.onSwipingLeft) this.props.onSwipingLeft(true);
-                if(this && this.props && this.props.left) this.props.left(true);
-            }
-        }
-        else{
-            if(Math.abs(tY)>=this.state.delta){
-                if(tY>this.state.delta){
-                    if(this && this.props && this.props.onSwipingDown) this.props.onSwipingDown(true);
-                }
-                else if(tY<(-this.state.delta)){
-                    if(this && this.props && this.props.onSwipingUp) this.props.onSwipingUp(true);
-                    if(this && this.props && this.props.up) this.props.up(true);
-                }
+        } else if (Math.abs(tY) >= this.state.delta) {
+            if (tY > this.state.delta) {
+                if (this && this.props && this.props.onSwipingDown) this.props.onSwipingDown(true);
+            } else if (tY < -this.state.delta) {
+                if (this && this.props && this.props.onSwipingUp) this.props.onSwipingUp(true);
+                if (this && this.props && this.props.up) this.props.up(true);
             }
         }
 
-        if(!this.state.detected){
-            if(Math.abs(parseFloat(tX))>=this.state.delta){
-                if(parseFloat(tX)>this.state.delta){
-                    if(this && this.props && this.props.onSwipedRight) this.props.onSwipedRight(true);
-                    this.setState({detected: true});
+        if (!this.state.detected) {
+            if (Math.abs(parseFloat(tX)) >= this.state.delta) {
+                if (parseFloat(tX) > this.state.delta) {
+                    if (this && this.props && this.props.onSwipedRight) this.props.onSwipedRight(true);
+                    this.setState({ detected: true });
+                } else if (parseFloat(tX) < -this.state.delta) {
+                    if (this && this.props && this.props.onSwipedLeft) this.props.onSwipedLeft(true);
+                    this.setState({ detected: true });
                 }
-                else if(parseFloat(tX)<(-this.state.delta)){
-                    if(this && this.props && this.props.onSwipedLeft) this.props.onSwipedLeft(true);
-                    this.setState({detected: true});
-                }
-            }
-            else{
-                if(Math.abs(parseFloat(tY))>=this.state.delta){
-                    if(parseFloat(tY)>this.state.delta){
-                        if(this && this.props && this.props.onSwipedDown) this.props.onSwipedDown(true);
-                        this.setState({detected: true});
-                    }
-                    else if(parseFloat(tY)<(-this.state.delta)){
-                        if(this && this.props && this.props.onSwipedUp) this.props.onSwipedUp(true);
-                        this.setState({detected: true});
-                    }
+            } else if (Math.abs(parseFloat(tY)) >= this.state.delta) {
+                if (parseFloat(tY) > this.state.delta) {
+                    if (this && this.props && this.props.onSwipedDown) this.props.onSwipedDown(true);
+                    this.setState({ detected: true });
+                } else if (parseFloat(tY) < -this.state.delta) {
+                    if (this && this.props && this.props.onSwipedUp) this.props.onSwipedUp(true);
+                    this.setState({ detected: true });
                 }
             }
         }
 
-        if(this.state.preventDefaultEvent) e.preventDefault();
-    },
+        if (this.state.preventDefaultEvent) e.preventDefault();
+    }
+
     _touchEnd(e) {
         this.setState({
             x: 0,
@@ -208,8 +187,26 @@ const Swipe = React.createClass({
             status: false,
             detected: false
         });
-        if(this.state.preventDefaultEvent) e.preventDefault();
+        if (this.state.preventDefaultEvent) e.preventDefault();
     }
-});
+}
+
+Swipe.propTypes = {
+    nodeName: PropTypes.string,
+    mouseSwipe: PropTypes.bool,
+    className: PropTypes.string,
+    onSwipingUp: PropTypes.func,
+    onSwipingRight: PropTypes.func,
+    onSwipingDown: PropTypes.func,
+    onSwipingLeft: PropTypes.func,
+    onSwipedUp: PropTypes.func,
+    onSwipedRight: PropTypes.func,
+    onSwipedDown: PropTypes.func,
+    onSwipedLeft: PropTypes.func,
+    onSwipe: PropTypes.func,
+    delta: PropTypes.number,
+    preventDefaultEvent: PropTypes.bool,
+    style: PropTypes.object
+};
 
 module.exports = Swipe;

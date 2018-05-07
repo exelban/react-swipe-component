f5d7f9eb3f9ab07e90481ca1978a7bc8c8c28ba8
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2018-05-07 22:39:12 +0200

    Update fix-flow-gen.sh

--- fix-flow-gen.sh
+++ fix-flow-gen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sed -i.gen 's/\(.\)set \([a-zA-Z0-9_]*[(]\)/\1; set \2/' ./lib/index.js.flow
-# sed -i.gen 's/anyset/any; set/' ./lib/index.js.flow
 sed -i.gen '/import.*$/ d' ./lib/index.js.flow
 sed -i.gen '2s/.*/\/* eslint-disable *\//' ./lib/index.js.flow
 sed -i.gen 's/declare export default class extends/declare export default class Swipe extends/' ./lib/index.js.flow

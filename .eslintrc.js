fb430d607d3a688ce6f127e6235ac9e89604936e
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2018-06-09 17:45:09 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,17 +27,16 @@ module.exports = {
   "rules": {
     "flowtype-errors/show-errors": 2,
 
-    "camelcase": "off", // Disallow to use camelcase in variables
-    "no-console": "off", // Allow to use console
-    "no-param-reassign": "off", // this is needed for store mutations
+    "camelcase": "off",
+    "no-console": "off",
+    "no-param-reassign": "off",
     "no-plusplus": "off",
     "no-underscore-dangle": "off",
 
-    "semi": ["error", "never"], // semicolon never at the end of line
+    "semi": ["error", "never"],
 
-    "space-before-function-paren": ["error", "always"], // enforce consistent spacing before function definition
+    "space-before-function-paren": ["error", "always"],
 
-    // let flow handle resolving - he is much better in that :)
     'import/no-extraneous-dependencies': 0,
     'import/no-unresolved': 0,
     'import/extensions': 0,
